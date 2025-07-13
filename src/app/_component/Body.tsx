aa2ae597a52653b851f542e3434e0a0106523adc
Shinya ( Shin , Shaun, Shun) <47809066+saitoshi@users.noreply.github.com>
2025-07-13 10:16:26 +0900

    issue-01: added messagebody files

---
+++ src/app/_component/Body.tsx
@@ -0,0 +1,13 @@
+import './style.css';
+ 
+export const Body = () => {
+    return (
+        <div id='chatBotBody'>
+            <div className='messageInner'>
+                <div className='messageDate'>
+                    2025/07/13
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Header } from "./_component/Header";
+import { Body } from "./_component/Body";
 export default function Home() {
   return (
     <div id="chatBotPanel">
-      <Header/>
+      <Header />
+      <Body/>
  </div>
   );
 }
