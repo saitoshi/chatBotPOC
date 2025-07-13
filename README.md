1a4c61ed8cdea7128feba26aae82fc463ccc11c6
Shinya ( Shin , Shaun, Shun) <47809066+saitoshi@users.noreply.github.com>
2025-07-13 10:06:05 +0900

    issue-00: added the header

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ This is a chatbot created for practice purposes.
 
 The design for the chatbot is replicated by the one using from the ANA website. 
 
+Link Used To Replicate - https://www.ana.co.jp/ja/jp/guide/amc/award/
+
 The following is the set up of the repository: 
 
 ```
---
+++ src/app/_component/Header.tsx
@@ -0,0 +1,12 @@
+import './style.css';
+
+export const Header = () => {
+    return (
+        <div className='chatBotHeader'>
+            <div className='chatBotInner'>
+            <span className='chatBotHeaderTitle'>ANA chat</span>
+            </div>
+       
+        </div>
+    )
+}
